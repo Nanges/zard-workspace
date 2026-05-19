1262c26b071acde95bc528de17c19cdc9b558952
Nanges <laha.olivier@gmail.com>
2026-05-20 01:58:31 +0200

    setup foo component

---
+++ projects/ui/components/foo/foo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-foo',
+  imports: [],
+  templateUrl: './foo.component.html',
+  styleUrl: './foo.component.css',
+})
+export class FooComponent {
+
+}
