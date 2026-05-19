4c6e184b4ec74beab6311ab5d8cc8c91ea059742
Nanges <laha.olivier@gmail.com>
2026-05-20 01:52:35 +0200

    setup ui

---
+++ projects/ui/src/lib/ui.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ui',
+  imports: [],
+  template: `
+    <p>
+      ui works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Ui {}
