fbe9aed6ef3f3ced785c86bd7ac694033f78b6ce
Nanges <laha.olivier@gmail.com>
2026-05-20 02:18:44 +0200

    Setup button

--- projects/dashboard/src/app/app.ts
+++ projects/dashboard/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ZardButtonComponent } from '@/ui/primitives/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ZardButtonComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
