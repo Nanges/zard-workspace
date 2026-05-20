07a2def6c4ef1650c2698647ae047566e28c9ce6
Nanges <laha.olivier@gmail.com>
2026-05-20 02:15:10 +0200

    setup zard part 1

--- projects/dashboard/src/app/app.ts
+++ projects/dashboard/src/app/app.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FooComponent } from '@/ui/components/foo';
 
 @Component({
   selector: 'app-root',
